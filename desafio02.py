0817039fbed90565428edb25866a914dc3d96046
tricia <--global>
2024-09-13 19:55:13 -0300

    atualizacao atvd

--- desafio02.py
+++ desafio02.py
@@ -1,12 +1,20 @@
-###senha, 3 tentativas, SEnha blOqueada
+#senha, 3 tentativas, senha bloqueada
 
 senha = 3184
-
-
+x = 0
+tent = 3
 usuario = int(input("Digite a senha de 4 digitos: "))
 
-for x in range(2):
-    if usuario != senha:
-        denovo = int(input("Tente mais uma vez:"))
-else:
-        print("BLOQUEADO!")
+while usuario != senha and tent > 1:
+    tent -= 1
+    if tent > 0:
+        print(f"Senha incorreta. Você tem {tent} tentativa(s) restante(s).")
+        usuario = int(input("Tente mais uma vez: "))
+    else:
+        print("Número bloqueado")
+
+
+if usuario == senha:
+    print("aprovado")
+
+
