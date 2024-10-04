80b67171e8d5ae60780c196600216b2695e18e59
tricia <--global>
2024-10-04 17:43:43 -0300

    2/2 desafioavancado1

--- desafio_avancado1.py
+++ desafio_avancado1.py
@@ -2,27 +2,41 @@ usuarios = []
 senhas = []
 
 def cadastro():
-      usuario = input("Digite o nome de usuario: ")
-      if usuario in usuario:
-            print("usuario cadastrado")
-      senha = int(input("Digite a senha numerica:"))
-      usuarios.append(usuario)
-      senhas.append(senha)
-      print("cadastro feito com sucesso")
+      while True:
+            usuario = input("Digite o nome de usuario: ")
+            if usuario in usuarios:
+                  print("usuario cadastrado")
+            else:
+                  senha = int(input("Digite a senha numerica:"))
+                  usuarios.append(usuario)
+                  senhas.append(senha)
+                  print("cadastro feito com sucesso")
+
+            dnv = int(input(f"Você deseja cadastrar mais um usuario?\n "
+                        f"(1- sim// 2- não) :"))
+            if dnv != 1:
+                  break
 
 def login():
       while True:
             usuario = input("Digite o nome de usuario: ")
             senha = int(input("Digite a senha numerica:"))
             if usuario in usuarios:
-                  for i in range(len(usuarios)):
-                        if usuarios[i] == usuario and senhas[i] == senha:
-                              print(f"Bem vindo, {usuario}")
-                              return
-                        else:
-                              print("usuario ou senhas incorretos")
-
+                  indice = usuarios.index(usuario)
+                  if senhas[indice] == senha:
+                        print(f"Bem-vindo, {usuario}!")
+                        break
+                  else:
+                        print("senha incorreta!")
+            else:
+                  print("Usuario invalido!")
 def mostrar():
+    print("Os usuários cadastrados e suas senhas são:")
+    for i in range(len(usuarios)):
+        print(f"Usuário: {usuarios[i]} - Senha: {senhas[i]}")
+
+def sair():
+      print("Finalizando...")
 
 def menu():
       while True:
